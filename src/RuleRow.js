da487a81b274328ce91ccfd802994740c369b856
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-06-17 11:01:30 +0200

    fixed bug to not allow choosing rule a second time

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
-import './RuleRow.css'
+import './RuleRow.css';
 
 class RuleRow extends Component {
-  render() {
-    return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
-        <td className="RuleRow-name">{this.props.name}</td>
-        <td className="RuleRow-score">{this.props.score}</td>
-      </tr>
-    )
-  }
+	render() {
+		return (
+			<tr
+				className={
+					this.props.score >= 0
+						? 'RuleRow RuleRow-disabled'
+						: 'RuleRow RuleRow-active'
+				}
+				onClick={this.props.score === undefined ? this.props.doScore : undefined}>
+				<td className='RuleRow-name'>{this.props.name}</td>
+				<td className='RuleRow-score'>{this.props.score}</td>
+			</tr>
+		);
+	}
 }
 
 export default RuleRow;
