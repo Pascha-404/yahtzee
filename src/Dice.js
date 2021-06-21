58af666999ca8fbf778244d56d5c21f6de40d4a1
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-06-21 15:27:58 +0200

    passed down isRolling & rollsLeft

--- src/Dice.js
+++ src/Dice.js
@@ -3,17 +3,23 @@ import Die from './Die';
 import './Dice.css';
 
 class Dice extends Component {
-  render() {
-    return <div className="Dice">
-      {this.props.dice.map((d, idx) =>
-        <Die handleClick={this.props.handleClick}
-          val={d}
-          locked={this.props.locked[idx]}
-          idx={idx}
-          key={idx} />
-      )}
-    </div>
-  }
+	render() {
+		return (
+			<div className='Dice'>
+				{this.props.dice.map((d, idx) => (
+					<Die
+						handleClick={this.props.handleClick}
+						val={d}
+						locked={this.props.locked[idx]}
+						idx={idx}
+						key={idx}
+						isRolling={this.props.isRolling}
+						rollsLeft={this.props.rollsLeft}
+					/>
+				))}
+			</div>
+		);
+	}
 }
 
 export default Dice;
