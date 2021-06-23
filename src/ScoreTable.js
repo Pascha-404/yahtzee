c2a969eeedddaf9d85ddfe7ebbe86e1a5b80d62a
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-06-23 20:29:35 +0200

    fixed bug / moved description in Rules.js

--- src/ScoreTable.js
+++ src/ScoreTable.js
@@ -20,21 +20,6 @@ import {
 class ScoreTable extends Component {
 	render() {
 		const { scores, doScore } = this.props;
-		const description = {
-			ones: '1 point per 1',
-			twos: '2 points per 2',
-			threes: '3 points per 3',
-			fours: '4 points per 4',
-			fives: '5 points per 5',
-			sixes: '6 points per 6',
-			threeOfKind: 'Sum all dice if 3 are the same',
-			fourOfKind: 'Sum all dice if 4 are the same',
-			fullHouse: '25 points for a full house',
-			smallStraight: '30 points for a small straight',
-			largeStraight: '40 points for a large straight',
-			yahtzee: '50 points for yahtzee',
-			chance: 'Sum of all dice',
-		};
 
 		return (
 			<div className='ScoreTable'>
@@ -46,37 +31,37 @@ class ScoreTable extends Component {
 								name='Ones'
 								score={scores.ones}
 								doScore={evt => doScore('ones', ones.evalRoll)}
-								description={description.ones}
+								description={ones.description}
 							/>
 							<RuleRow
 								name='Twos'
 								score={scores.twos}
 								doScore={evt => doScore('twos', twos.evalRoll)}
-								description={description.twos}
+								description={twos.description}
 							/>
 							<RuleRow
 								name='Threes'
 								score={scores.threes}
 								doScore={evt => doScore('threes', threes.evalRoll)}
-								description={description.threes}
+								description={threes.description}
 							/>
 							<RuleRow
 								name='Fours'
 								score={scores.fours}
 								doScore={evt => doScore('fours', fours.evalRoll)}
-								description={description.fours}
+								description={fours.description}
 							/>
 							<RuleRow
 								name='Fives'
 								score={scores.fives}
 								doScore={evt => doScore('fives', fives.evalRoll)}
-								description={description.fives}
+								description={fives.description}
 							/>
 							<RuleRow
 								name='Sixes'
 								score={scores.sixes}
 								doScore={evt => doScore('sixes', sixes.evalRoll)}
-								description={description.sixes}
+								description={sixes.description}
 							/>
 						</tbody>
 					</table>
@@ -90,43 +75,43 @@ class ScoreTable extends Component {
 								name='Three of Kind'
 								score={scores.threeOfKind}
 								doScore={evt => doScore('threeOfKind', threeOfKind.evalRoll)}
-								description={description.threeOfKind}
+								description={threeOfKind.description}
 							/>
 							<RuleRow
 								name='Four of Kind'
 								score={scores.fourOfKind}
 								doScore={evt => doScore('fourOfKind', fourOfKind.evalRoll)}
-								description={description.fourOfKind}
+								description={fourOfKind.description}
 							/>
 							<RuleRow
 								name='Full House'
 								score={scores.fullHouse}
 								doScore={evt => doScore('fullHouse', fullHouse.evalRoll)}
-								description={description.fullHouse}
+								description={fullHouse.description}
 							/>
 							<RuleRow
 								name='Small Straight'
 								score={scores.smallStraight}
 								doScore={evt => doScore('smallStraight', smallStraight.evalRoll)}
-								description={description.smallStraight}
+								description={smallStraight.description}
 							/>
 							<RuleRow
 								name='Large Straight'
 								score={scores.largeStraight}
 								doScore={evt => doScore('largeStraight', largeStraight.evalRoll)}
-								description={description.largeStraight}
+								description={largeStraight.description}
 							/>
 							<RuleRow
 								name='Yahtzee'
 								score={scores.yahtzee}
 								doScore={evt => doScore('yahtzee', yahtzee.evalRoll)}
-								description={description.yahtzee}
+								description={yahtzee.description}
 							/>
 							<RuleRow
 								name='Chance'
 								score={scores.chance}
 								doScore={evt => doScore('chance', chance.evalRoll)}
-								description={description.chance}
+								description={chance.description}
 							/>
 						</tbody>
 					</table>
